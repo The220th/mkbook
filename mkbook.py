9eb486457e924271c64c244ebf381186e58c9f39
Your Name <you@example.com>
2023-06-02 17:12:12 +0300

    README.md

--- mkbook.py
+++ mkbook.py
@@ -23,6 +23,14 @@ def init_args(args: list) -> "ArgumentParser":
                         help="Rotates output pages with even numbers by 180 degrees.")
     parser.add_argument("--out_one_by_one", default=False, action='store_true',
                         help="The output pages go one after the other, not the even ones first, and then the odd ones.")
+    parser.add_argument("--save_odd_even", default=False, action='store_true',
+                        help="Additionally save pages with odd and even numbers separately.")
+    parser.add_argument("--add_blank_before", type=int, default=0,
+                        help="Add blank sheets to the beginning.")
+    parser.add_argument("--add_blank_after", type=int, default=0,
+                        help="Add blank sheets to the end.")
+    parser.add_argument("--out_landscape", default=False, action='store_true',
+                        help="The output will be in landscape mode.")
     args = parser.parse_args(args[1:])
 
     return args
@@ -50,6 +58,20 @@ def solve_transform(imgs: list, args: list) -> list:
     pout("OK")
 
     return res
+
+def adding_blank(imgs: list, before: int, after: int) -> list:
+    blank = ImageHundler(imgs[0])
+    blank = blank.get_as_empty()
+
+    res_before = []
+    for i in range(before):
+        res_before.append(blank.copy())
+    
+    res_after = []
+    for i in range(after):
+        res_after.append(blank.copy())
+
+    return res_before + imgs + res_after
 
 def fill_4(imgs: list) -> list:
     c = len(imgs)
@@ -79,6 +101,11 @@ def rotate_even_180(imgs: list) -> list:
     for i, img_i in enumerate(imgs):
         imgs[i] = imgs[i].rotate(180, expand=True)
     pout("OK")
+    return imgs
+
+def rotate_90(imgs: list) -> list:
+    for i, img_i in enumerate(imgs):
+        imgs[i] = imgs[i].rotate(90, expand=True)
     return imgs
 
 def A5_A4(imgs: list, odd_even: bool = False) -> list:
@@ -111,10 +138,6 @@ def pout(s: str, endl: bool = True):
         print(s)
     sys.stdout.flush()
 
-# TODO: Добавить сохранение отдельно нечет/чет
-# TODO: Добавит возможно добавление пустых страниц как в начале, так и в конце
-#       Так и в итоговом варианте.
-
 if __name__ == '__main__':
     """
     1. Получить все листы из pdf, как png.
@@ -130,17 +153,25 @@ if __name__ == '__main__':
     pdf_out = os.path.abspath(args.pdf_out[0])
     rotate180_even = args.rotate180_even
     out_one_by_one = args.out_one_by_one
+    save_odd_even = args.save_odd_even
+    out_landscape = args.out_landscape
+    add_blank_before, add_blank_after = args.add_blank_before, args.add_blank_after
+    if(add_blank_before < 0 or add_blank_after < 0):
+        pout(f"add_blank_before={add_blank_before} and add_blank_after={add_blank_after} must be non negative. ")
+        exit()
     dpi = args.dpi
 
     pout(f"Getting from pdf \"{pdf_in}\" pages with dpi={dpi}... " , endl=False)
     imgs = pdf_convert_from_path(pdf_in, dpi)
     pout("OK")
     if(len(imgs) <= 0):
-        print(f"\"pdf_in\" has {len(imgs)} pages. Exitting...")
+        pout(f"\"pdf_in\" has {len(imgs)} pages. Exitting...")
         exit()
 
     imgs = solve_transform(imgs, args)
     
+    imgs = adding_blank(imgs, add_blank_before, add_blank_after)
+
     imgs = fill_4(imgs)
 
     imgs_odd, imgs_even = split(imgs)
@@ -160,8 +191,32 @@ if __name__ == '__main__':
         imgs = [imgs_odd[i//2] if i%2==0 else imgs_even[i//2] for i in range(len(imgs_odd)+len(imgs_even))]
         pout("OK")
 
+    if(out_landscape == False):
+        pout("Rotating all pages by 90 degrees to form an output in portrait mode... ", endl=False)
+        imgs = rotate_90(imgs)
+        pout("OK")
+        if(save_odd_even == True):
+            pout("Rotating odd pages by 90 degrees to form an output in portrait mode... ", endl=False)
+            imgs_odd = rotate_90(imgs_odd)
+            pout("OK")
+
+            pout("Rotating even pages by 90 degrees to form an output in portrait mode... ", endl=False)
+            imgs_even = rotate_90(imgs_even)
+            pout("OK")
+
     pout(f"Saving output pdf \"{pdf_out}\"... ", endl=False)
     imgs[0].save(pdf_out, save_all=True, append_images=imgs[1:])
     pout("OK")
+
+    if(save_odd_even == True):
+        fn, fe = os.path.splitext(pdf_out)
+        odds_path, evens_path = fn + "_odds" + fe, fn + "_evens" + fe
+        pout(f"Saving odds \"{odds_path}\"... ", endl=False)
+        imgs_odd[0].save(odds_path, save_all=True, append_images=imgs_odd[1:])
+        pout("OK")
+        pout(f"Saving odds \"{evens_path}\"... ", endl=False)
+        imgs_even[0].save(evens_path, save_all=True, append_images=imgs_even[1:])
+        pout("OK")
+
 
     pout(f"{'='*20}DONE!{'='*20}")
